c037c25c9b903d97d8684e214df6e6571afdba12
ChengHeo <126269413+ChengHeo@users.noreply.github.com>
2023-03-10 13:14:39 +0800

    Add files via upload

---
+++ Exercise_01/Coding_01.py
@@ -0,0 +1,252 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=3
+b=5
+print(a*b**2)
+
+
+# In[2]:
+
+
+d=5
+d-=1
+print(d,d+1,d-2)
+
+
+# In[3]:
+
+
+n=5
+n**2
+print(n/5)
+
+
+# In[4]:
+
+
+points=30
+points+=20*10
+print(points)
+
+
+# In[5]:
+
+
+totalBerries=100
+totalCost=352
+eachBerry=totalCost/totalBerries
+print(eachBerry)
+
+
+# In[7]:
+
+
+totalMeters=30255
+kiloMeters=totalMeters//1000
+meters=totalMeters%1000
+print(kiloMeters,meters)
+
+
+# In[8]:
+
+
+"Python"[4]
+
+
+# In[9]:
+
+
+"Python"[-2]
+
+
+# In[10]:
+
+
+"Hello Python"[-9]
+
+
+# In[11]:
+
+
+"Python"[5]
+
+
+# In[12]:
+
+
+"Python"[0:3]
+
+
+# In[13]:
+
+
+"Python"[2:2]
+
+
+# In[14]:
+
+
+"Python"[:2]
+
+
+# In[15]:
+
+
+"Python"[2:]
+
+
+# In[16]:
+
+
+"Python"[-3:-2]
+
+
+# In[17]:
+
+
+"Python"[-5:-1]
+
+
+# In[18]:
+
+
+"Python"[2:-2]
+
+
+# In[19]:
+
+
+"Python"[-4:4]
+
+
+# In[20]:
+
+
+"Python"[:]
+
+
+# In[21]:
+
+
+"Python"[-10:10]
+
+
+# In[22]:
+
+
+"Python".find("tho")
+
+
+# In[23]:
+
+
+"Python".find("ty")
+
+
+# In[24]:
+
+
+"Python".find("oh")
+
+
+# In[25]:
+
+
+"Python".find("Pyt")
+
+
+# In[26]:
+
+
+"whizzbuzz".rfind("zz")
+
+
+# In[27]:
+
+
+"whizzbuzz".find("zz")
+
+
+# In[28]:
+
+
+"Python".lstrip()
+
+
+# In[29]:
+
+
+"hello_world".startswith("hell")
+
+
+# In[30]:
+
+
+print("merry","christmas",'!',sep="")
+
+
+# In[32]:
+
+
+print("Price:",'$',23.45,sep="")
+
+
+# In[33]:
+
+
+print("Portion:",90,'%',sep="")
+
+
+# In[34]:
+
+
+print("Py","th","on",sep="")
+
+
+# In[35]:
+
+
+print(1,2,3,sep="x")
+
+
+# In[36]:
+
+
+print("tic","tac","toe",sep='-')
+
+
+# In[37]:
+
+
+print("father","in","law",sep='-')
+
+
+# In[38]:
+
+
+print("one","two","three",sep=',')
+
+
+# In[40]:
+
+
+print("What is your name",end='?\n')
+print("John")
+
+
+# In[42]:
+
+
+print("spam",end=" and ")
+print("eggs")
+
+
+# In[ ]:
+
+
+
+
